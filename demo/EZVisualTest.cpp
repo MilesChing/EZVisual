234d0154991e05bf7ed9b00f4faf3d7bf3e5020e
MilesChing <qinzhengrui@live.cn>
2019-09-19 16:28:01 +0800

    Complete the new demo.

--- demo/EZVisualTest.cpp
+++ demo/EZVisualTest.cpp
@@ -1,3 +1,6 @@
+/* Demo of EZVisual
+ * This demo is a blackboard application where you can switch colors or paint with a mouse.
+ */
 #include <iostream>
 #include <string>
 #include <thread>
@@ -9,40 +12,73 @@ using namespace std;
 using namespace rapidjson;
 using namespace EZVisual;
 using namespace cv;
-#define debug(a) cerr<<#a<<" = "<<a<<endl
+
+EZVisual::Border* borders[4];
+EZVisual::Canvas* canvas;
+int current_index = 0;
+EZVisual::Color current_color;
+bool drawing = false;
+
+//Check this path if you get a "file not exist" exception.
+string visual_profile_path = "EZVisual_Demo.json";
 
 int main(){
-    try{
-        Visualization vis("/home/milesching/workspace/EZVisual/demo/EZVisual_Demo.json");
+    //Create visualization object
+    Visualization vis(visual_profile_path);
+    //Launch up a window
+    thread t(&Visualization::LaunchWindow, &vis);
+    vis.Invoke([&](Visualization* visualization){
+        for(int i = 0; i < 4; ++i){
+            //Store Border in borders
+            //You could only store pointers to visual elements outside function "Invoke"
+            //Modifing outside is not permitted
+            borders[i] = visualization->GetVisualElement<Border>(i + 10);
 
-        thread t(&Visualization::LaunchWindow, &vis);
+            //Register listeners
+            borders[i]->AddMouseListener(MouseLeftDown, [&](const EZVisual::MouseEventParameter& param){
+                //Don't use i here
+                if(current_index == param.sender->GetId() - 10) return;
+                //We need another Invoke because these codes are in a listener
+                //Thess invoking won't cause deadlock
+                //Use vis not visualization here
+                vis.Invoke([&](Visualization* visualization){
+                    Border* target = visualization->GetVisualElement<Border>(current_index);
+                    target->SetBackground(0);
+                });
+                current_index = param.sender->GetId() - 10;
+                //Update the color of the selected border
+                vis.Invoke([&](Visualization* visualization){
+                    Border* target = visualization->GetVisualElement<Border>(current_index);
+                    target->SetBackground(0xffffffff);
+                    target = visualization->GetVisualElement<Border>(current_index + 10);
+                    current_color = target->GetBackground();
+                });
+            });
+        }
 
-        bool drawing = false;
+        //Register listener for canvas
+        canvas = visualization->GetVisualElement<Canvas>(16);
 
-        vis.Invoke<EZVisual::Canvas>(16, [&drawing](EZVisual::Canvas* canvas){
-            canvas->AddMouseListener(MouseLeftDown, [&drawing](const MouseEventParameter& param){
-                drawing = true;
-            });
-            canvas->AddMouseListener(MouseLeftUp, [&drawing](const MouseEventParameter& param){
-                drawing = false;
-            });
-            canvas->AddMouseListener(MouseMoving, [&drawing, &canvas](const MouseEventParameter& param){
-                if(drawing){
-                    canvas->PaintPixel(make_pair(param.relative_x, param.relative_y), 0xffffffff);
-                }
-            });
+        //Set drawing mode when left button pushed.
+        canvas->AddMouseListener(MouseLeftDown, [&](const EZVisual::MouseEventParameter& param){
+            drawing = true;
         });
 
-        t.join();
-    }
-    catch(string s){
-        cout << s;
-    }
-    catch(const char* s){
-        cout << s;
-    }
-    catch(double s){
-        cout << s;
-    }
+        canvas->AddMouseListener(MouseLeftUp, [&](const EZVisual::MouseEventParameter& param){
+            drawing = false;
+        });
+
+        //Draw points
+        canvas->AddMouseListener(MouseMoving, [&](const EZVisual::MouseEventParameter& param){
+            if(!drawing) return;
+            vis.Invoke([&](Visualization* visualization){
+                canvas->PaintCircle(make_pair(param.relative_x, param.relative_y), 2, current_color, 0, 0, 0);
+            });
+        });
+    });
+
+    //Window will be waiting for key 27 by default
+    t.join();
+
     return 0;
 }
